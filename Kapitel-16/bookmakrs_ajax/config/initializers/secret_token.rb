aa0a6f1fbe254de28c731e0f7c081312cc318fe2
Hussein Morsy <hussein@morsy.de>
2011-11-30 08:52:28 +0100

    Kapitel 16: bookmarks_ajax Beispiel hinzugefügt

---
+++ Kapitel-16/bookmakrs_ajax/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmakrsAjax::Application.config.secret_token = '761596a40e304c923d435c06f7cd3102fbd88037c3955cb65917d096282afe29e95923c6d0bb43e916af38ed11b8460e1fbfdd1b435db97e99f9a09166caa65f'
